7bffeaca44b77396066adef6c1e4b41280d5cc7a
Rashmi-Ranjan-Prusty <130736738+Rashmi-Ranjan-Prusty@users.noreply.github.com>
2024-10-15 23:00:17 +0530

    added ArticleFragment and news file

--- app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -1,10 +1,11 @@
-package com.example.thenewsapp
+package com.example.thenewsapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.thenewsapp.R
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
